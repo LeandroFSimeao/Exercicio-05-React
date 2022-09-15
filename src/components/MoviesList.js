67ba0f135d848a70afcd2eeaed504da2ea4730ea
Leandro Simeao <leandrosimeao@yahoo.com.br>
2022-09-15 16:40:56 -0300

    Adicionando Redux

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -4,7 +4,7 @@ export function MoviesList({ movies }) {
 	return (
 		<div className="container">
 			{movies.map((movie) => (
-				<Movie movieId={movie.id} movieTitle={movie.title} overview={movie.overview} />
+				<Movie key={movie.id} movieId={movie.id} movieTitle={movie.title} overview={movie.overview} />
 			))}
 		</div>
 	);
--- src/routes/ApplicationRoutes.js
+++ src/routes/ApplicationRoutes.js
@@ -1,6 +1,7 @@
 import React, { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 import { Home } from "../views/Home";
+import { Playlist } from "../views/Playlist";
 const MovieDetail = lazy(() => import(/* webpackChunkName: "movieDetail" */ "../views/MovieDetail"));
 
 export function ApplicationRoutes() {
@@ -9,6 +10,7 @@ export function ApplicationRoutes() {
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/movie/:movieId" element={<MovieDetail />} />
+				<Route path="playlist" element={<Playlist />} />
 			</Routes>
 		</Suspense>
 	);
